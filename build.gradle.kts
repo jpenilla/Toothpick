ad7a8065b5b8b7ce8e10f52d035c7e4dc9e7e2eb
jmp <jasonpenilla2@me.com>
2021-05-03 04:04:30 -0700

    Build updates
    Also bump Shadow to 7.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
-  `maven-publish`
   `kotlin-dsl`
-  id("net.kyori.indra.license-header") version "2.0.2"
+  id("com.gradle.plugin-publish")
+  id("net.kyori.indra")
+  id("net.kyori.indra.license-header")
+  id("net.kyori.indra.publishing.gradle-plugin")
 }
 
 group = "xyz.jpenilla"
 version = "1.0.0-SNAPSHOT"
+description = "Gradle plugin to assist in forking Paper"
 
 repositories {
   mavenCentral()
@@ -13,21 +16,9 @@ repositories {
 }
 
 dependencies {
-  val jacksonVersion = "2.12.2"
-  implementation("com.fasterxml.jackson.dataformat", "jackson-dataformat-xml", jacksonVersion)
-  implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", jacksonVersion)
-
-  val configurateVersion = "4.1.1"
-  implementation("org.spongepowered", "configurate-hocon", configurateVersion)
-  implementation("org.spongepowered", "configurate-extra-kotlin", configurateVersion)
-
-  implementation("gradle.plugin.com.github.jengelman.gradle.plugins", "shadow", "7.0.0")
-}
-
-java {
-  sourceCompatibility = JavaVersion.toVersion(8)
-  targetCompatibility = JavaVersion.toVersion(8)
-  withSourcesJar()
+  implementation(libs.bundles.jackson)
+  implementation(libs.bundles.configurate)
+  implementation(libs.shadow)
 }
 
 kotlin {
@@ -41,43 +32,32 @@ tasks {
   }
 }
 
-gradlePlugin {
-  plugins {
-    create("Toothpick") {
-      id = "xyz.jpenilla.toothpick"
-      implementationClass = "xyz.jpenilla.toothpick.Toothpick"
-    }
+indra {
+  javaVersions {
+    target(8)
   }
-}
-
-extensions.getByType<PublishingExtension>().publications.withType<MavenPublication>().configureEach {
-  pom {
-    name.set(project.name)
-    description.set(project.description)
-    url.set("https://github.com/jpenilla/Toothpick")
-
-    developers {
-      developer {
-        id.set("jmp")
-        timezone.set("America/Los Angeles")
-      }
-    }
-
-    licenses {
-      license {
-        name.set("MIT")
-        url.set("https://github.com/jpenilla/Toothpick/raw/master/license.txt")
-        distribution.set("repo")
+  mitLicense()
+  github("jpenilla", "Toothpick")
+  publishSnapshotsTo("jmp", "https://repo.jpenilla.xyz/snapshots")
+  configurePublications {
+    pom {
+      developers {
+        developer {
+          id.set("jmp")
+          timezone.set("America/Los Angeles")
+        }
       }
     }
   }
 }
 
-publishing.repositories.maven {
-  url = if (project.version.toString().endsWith("-SNAPSHOT")) {
-    uri("https://repo.jpenilla.xyz/snapshots")
-  } else {
-    uri("https://repo.jpenilla.xyz/releases")
-  }
-  credentials(PasswordCredentials::class)
+indraPluginPublishing {
+  plugin(
+    "toothpick",
+    "xyz.jpenilla.toothpick.Toothpick",
+    "Toothpick",
+    project.description,
+    listOf("minecraft", "paper", "forking", "patching")
+  )
+  website("https://github.com/jpenilla/Toothpick")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
+enableFeaturePreview("VERSION_CATALOGS")
+
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    maven("https://repo.stellardrift.ca/repository/snapshots/")
+  }
+}
+
+plugins {
+  id("ca.stellardrift.polyglot-version-catalogs") version "5.0.0-SNAPSHOT"
+}
+
 rootProject.name = "toothpick"
